e3638402e0a8061581195626a76f7c87f4557559
xLiQuiDz <45235952+xLiQuiDz@users.noreply.github.com>
2021-07-23 10:30:55 +0200

    Base

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,9 +1,5 @@
 from envs.predatorprey_wrapper import PredatorPreyWrapper
-from envs.smac_wrapper import SMACWrapper
-from envs.traffic_junction_wrapper import TrafficJunctionWrapper
 
 __all__ = [
-    'PredatorPreyWrapper',
-    'SMACWrapper',
-    'TrafficJunctionWrapper',
+    'PredatorPreyWrapper'
 ]
